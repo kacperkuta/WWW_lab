6a97733efd527c7f7acde77d8cfd23e25aa9a79c
Kacper Kuta <k.kuta2@student.uw.edu.pl>
2020-03-25 12:35:24 +0100

    Init commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+let jsonString: string = `{
+    "piloci": [
+        "Pirx",
+        "Exupery",
+        "Idzikowski",
+        "Główczewski"
+    ],
+    "lotniska": {
+        "WAW": ["Warszawa", [3690, 2800]],
+        "NRT": ["Narita", [4000, 2500]],
+        "BQH": ["Biggin Hill", [1802, 792]],
+        "LBG": ["Paris-Le Bourget", [2665, 3000, 1845]]
+    }
+}`;
+
+function zaloguj(...komunikaty: string[]) {
+    console.log("Ależ skomplikowany program!", ...komunikaty)
+}
+
+zaloguj("Ja", "cię", "nie", "mogę");
